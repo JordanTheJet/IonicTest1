500b8ff594ca51b00a1fe6cc6ccb45dd1862017c
JordanTheJet <morepencils@gmail.com>
2018-12-04 08:50:32 -0600

    Add files via upload
    Ionic testing from 12/3 due 12/4

---
+++ home/home.page.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+constructor(){
+
+}
+catpower(){
+  console.log("This is cat power")
+  
+}
+}
